c7270aa528952b5dae4d8eaa19ef3789d77a6493
douglasspeck <speck.empresarial@gmail.com>
2021-12-02 23:32:12 -0300

    Started Homepage

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
     <?php
         $title = 'DaBlue | Um presente para meu maior presente';
         $keywords = 'DaBlue, Blue, Bastos, Deschamps';
-        $css = ['homepage', 'homepage-mobile'];
+        $css = ['homepage', 'homepage-mobile', 'stars'];
         $js = [];
         include 'PHP/head.php';
     ?>
     <body>
-    
-</body>
+        <section id="background">
+            <div class="stars"></div>
+            <div class="twinkling"></div>
+            <div class="clouds"></div>
+        </section>
+        <section id="welcome-screen">
+            <div class="neptune"></div>
+            <div class="welcome-text"></div>
+        </section>
+    </body>
 </html>
