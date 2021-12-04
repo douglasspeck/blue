d58f1cdc52a040d8878954c3bd67291d4b679efb
douglasspeck <speck.empresarial@gmail.com>
2021-12-04 00:50:27 -0300

    Finished Homepage Base

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <?php
         $title = 'DaBlue | Um presente para meu maior presente';
         $keywords = 'DaBlue, Blue, Bastos, Deschamps';
-        $css = ['homepage', 'homepage-mobile', 'stars'];
+        $css = ['main','fonts','homepage', 'homepage-mobile', 'stars'];
         $js = [];
         include 'PHP/head.php';
     ?>
@@ -14,8 +14,14 @@
             <div class="clouds"></div>
         </section>
         <section id="welcome-screen">
-            <div class="neptune"></div>
-            <div class="welcome-text"></div>
+            <div class="cat"></div>
+            <div class="clouds"></div>
+            <div class="neptune">
+                <img class="house" src="IMG/house-colorified.png"></img>
+            </div>
+            <div class="welcome-text">
+                <h1 class="m-title center">Bem-vindo a Netuno!</h1>
+            </div>
         </section>
     </body>
 </html>
